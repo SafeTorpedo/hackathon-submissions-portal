612192a2a5f7852a83a7d8a5d7b1c7f36494f307
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-03-22 22:19:58 +0530

    feat: Added Tab Menu

---
+++ src/components/TabMenu.tsx
@@ -0,0 +1,54 @@
+import { TabContext } from "@mui/lab";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Tab from "@mui/material/Tab";
+import { useState } from "react";
+
+const TabMenu = () => {
+    const [value, setValue] = useState("1");
+    const [order, setOrder] = useState("Newest");
+    const [search, setSearch] = useState("");
+    const handleChange = (e: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
+    return (
+        <div className="mt-[57px] ml-[140px]">
+            <div className="grid grid-cols-2">
+                <div>
+                    <TabContext value={value}>
+                        <TabList
+                            value={value}
+                            onChange={handleChange}
+                            textColor="primary"
+                            indicatorColor="primary"
+                        >
+                            <Tab label="All Submissions" value="1" />
+                            <Tab label="Favourite Submissions" value="2" />
+                        </TabList>
+                        <TabPanel value="1">One</TabPanel>
+                        <TabPanel value="2">Two</TabPanel>
+                    </TabContext>
+                </div>
+                <div>
+                    <input
+                        className=" border border-black rounded-xl w-[236px] h-[26px] ml-[45px] mt-2 p-[11px] font-[Poppins] text-sm"
+                        type="text"
+                        placeholder="Search"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <select
+                        className="rounded-xl w-[120px] h-[26px] ml-[56px] border border-black text-sm p-[2px]"
+                        value={order}
+                        onChange={(e) => setOrder(e.target.value)}
+                    >
+                        <option value="Newest">Newest</option>
+                        <option value="Oldest">Oldest</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TabMenu;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import TabMenu from "../components/TabMenu";
 
 const Home = () => {
     return (
         <>
             <Navbar />
             <Hero />
+            <TabMenu />
         </>
     );
 };
