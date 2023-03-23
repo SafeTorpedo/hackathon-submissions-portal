c52dcb893336097cf12c3bbf774036cc010beefb
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-03-23 19:39:21 +0530

    feat: Added project delete functionality

--- src/components/InfoHero.tsx
+++ src/components/InfoHero.tsx
@@ -1,31 +1,80 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useNavigate } from "react-router-dom";
+
 const InfoHero = ({ title, coverImage, summary, id }) => {
+    const navigate = useNavigate();
+    const [show, setShow] = useState(false);
+    const handleClose = () => {
+        setShow(false);
+    };
+    const handleDelete = () => {
+        //delete from local storage having title
+        const data = JSON.parse(localStorage.getItem("data") || "[]");
+        const newData = data.filter((value) => value.title !== title);
+        localStorage.setItem("data", JSON.stringify(newData));
+        navigate("/");
+    };
     return (
-        <div className=" bg-[#003145] h-[416px] w-full ">
-            <div className="grid grid-cols-2 pt-[96px] pl-[142px]">
-                <div className="grid grid-cols-2">
-                    <img src={coverImage} className="w-[120px] h-[120px]" />
-                    <h1 className="text-white font-semibold text-[42px] mt-4 -ml-20">
-                        {title}
-                    </h1>
-                </div>
-                <div className="text-white pl-96">
-                    <button className="w-[100px] h-[40px] rounded-lg border border-white">
-                        Edit
-                    </button>
-                    <br />
-                    <button className="mt-4 w-[100px] h-[40px] rounded-lg border border-white">
+        <>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Delete Model?
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>
+                        This action is irreversible. Are you sure you want to
+                        delete this model?
+                    </p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleClose} variant="alert">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleDelete} variant="alert">
                         Delete
-                    </button>
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <div className=" bg-[#003145] h-[416px] w-full ">
+                <div className="grid grid-cols-2 pt-[96px] pl-[142px]">
+                    <div className="grid grid-cols-2">
+                        <img src={coverImage} className="w-[120px] h-[120px]" />
+                        <h1 className="text-white font-semibold text-[42px] mt-4 -ml-20">
+                            {title}
+                        </h1>
+                    </div>
+                    <div className="text-white pl-96">
+                        <button className="w-[100px] h-[40px] rounded-lg border border-white">
+                            Edit
+                        </button>
+                        <br />
+                        <button
+                            onClick={() => setShow(true)}
+                            className="mt-4 w-[100px] h-[40px] rounded-lg border border-white"
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </div>
+                <p className="text-white mt-[24px] ml-[142px] w-[1156px] h-[26px]">
+                    {summary}
+                </p>
+                <div className="grid grid-cols-2 text-white mt-5">
+                    <h1 className="ml-[142px] ">S</h1>
+                    <h1 className="-ml-[450px]">{id.slice(0, 10)}</h1>
                 </div>
             </div>
-            <p className="text-white mt-[24px] ml-[142px] w-[1156px] h-[26px]">
-                {summary}
-            </p>
-            <div className="grid grid-cols-2 text-white mt-5">
-                <h1 className="ml-[142px] ">S</h1>
-                <h1 className="-ml-[450px]">{id.slice(0, 10)}</h1>
-            </div>
-        </div>
+        </>
     );
 };
 
